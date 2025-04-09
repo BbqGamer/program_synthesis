0ba25a7d9b8666282206ccd21b8807246bf0e666
BbqGamer <korba.adam@gmail.com>
2025-04-09 15:20:30 +0200

    Separate assembly game from Processor emulator

--- assembly_game/environments.py
+++ assembly_game/environments.py
@@ -1,49 +1,8 @@
-from enum import Enum
 from typing import Any
 
 import gymnasium
 
-
-class Instruction(Enum):
-    MOV = 0
-    CMP = 1
-    CMOVG = 2
-    RET = 3
-
-
-class Operand(Enum):
-    RDI = 0
-    RSI = 1
-    RAX = 2
-
-
-ProcState = tuple[
-    int,  # rdi
-    int,  # rsi
-    int,  # rax
-    int,  # comparison result
-]
-
-
-def evaluate_instruction(
-    state: ProcState, inst: Instruction, op1: Operand, op2: Operand
-) -> ProcState:
-    if inst == Instruction.MOV:
-        new_state = list(state)
-        new_state[op2.value] = new_state[op1.value]
-    elif inst == Instruction.CMP:
-        new_state = list(state)
-        new_state[3] = new_state[op2.value] - new_state[op1.value]
-    elif inst == Instruction.CMOVG:
-        new_state = list(state)
-        if new_state[3] > 0:
-            new_state[op2.value] = new_state[op1.value]
-    elif inst == Instruction.RET:
-        new_state = state
-    else:
-        raise ValueError("Invalid instruction")
-    return ProcState(tuple(new_state))
-
+from assembly_game.processor import Processor
 
 MIN = 1
 MAX = 2
@@ -57,20 +16,27 @@ class Min2Game(gymnasium.Env):
         # state are two possible arrangements of the registers
         # rdi and rsi are the two numbers to compare
         # rax is the result of the comparison
-        self.state = ((MIN, MAX, 0, 0), (MAX, MIN, 0, 0))
+        self.processors = [Processor(rdi=MIN, rsi=MAX), Processor(rdi=MAX, rsi=MIN)]
         self.t = 0
-        return self.state, {}
+
+        state = []
+        for proc in self.processors:
+            state.extend(proc.get_state())
+
+        return state, {}
 
     def step(self, action: Any) -> tuple[Any, float, bool, bool, dict[str, Any]]:
-        inst, op1, op2 = action
-        self.state = [evaluate_instruction(s, inst, op1, op2) for s in self.state]
+        for proc in self.processors:
+            halted = proc.evaluate_action(action)
+
         self.t += 1
-
-        num_correct = len(
-            list(filter(lambda s: s[Operand.RAX.value] == MIN, self.state))
-        )
-
-        done = self.t >= TIMEOUT or inst == Instruction.RET
+        num_correct = 0
+        state = []
+        for proc in self.processors:
+            state.extend(proc.get_state())
+            if proc.rax == 1:
+                num_correct += 1
 
         reward = 10 * num_correct - self.t
-        return self.state, reward, done, False, {}
+
+        return state, reward, halted, False, {}
---
+++ assembly_game/processor.py
@@ -0,0 +1,82 @@
+from enum import Enum
+
+
+class Instruction(Enum):
+    MOV = 0
+    CMP = 1
+    CMOVG = 2
+    RET = 3
+
+
+class Operand(Enum):
+    RDI = 0
+    RSI = 1
+    RAX = 2
+
+
+class Processor:
+    rdi: int
+    rsi: int
+    rax: int
+    cmp_res: int
+
+    actions = [
+        (inst, op1, op2)
+        for inst in [Instruction.MOV, Instruction.CMP, Instruction.CMOVG]
+        for op1 in Operand
+        for op2 in Operand
+    ] + [(Instruction.RET,)]
+
+    def __init__(self, rdi: int = 0, rsi: int = 0) -> None:
+        self.rdi = rdi
+        self.rsi = rsi
+        self.rax = 0
+        self.cmp_res = 0
+
+    def _set_reg(self, reg: Operand, value: int) -> None:
+        if reg == Operand.RDI:
+            self.rdi = value
+        elif reg == Operand.RSI:
+            self.rsi = value
+        elif reg == Operand.RAX:
+            self.rax = value
+        else:
+            raise ValueError("Invalid register")
+
+    def _get_reg(self, reg: Operand) -> int:
+        if reg == Operand.RDI:
+            return self.rdi
+        elif reg == Operand.RSI:
+            return self.rsi
+        elif reg == Operand.RAX:
+            return self.rax
+        else:
+            raise ValueError("Invalid register")
+
+    def n_actions(self) -> int:
+        return len(self.actions)
+
+    def evaluate_action(self, action_id: int) -> bool:
+        action = self.actions[action_id]
+        if len(action) == 1:
+            if action[0] != Instruction.RET:
+                raise ValueError("Unsupported instruction")
+            # doesn't matter what is in op1 and op2, RET just exits
+            return True
+        elif len(action) == 3:
+            inst, op1, op2 = action
+            if inst == Instruction.MOV:
+                self._set_reg(op2, self._get_reg(op1))
+            elif inst == Instruction.CMP:
+                self.cmp_res = self._get_reg(op2) - self._get_reg(op1)
+            elif inst == Instruction.CMOVG:
+                if self.cmp_res > 0:
+                    self._set_reg(op2, self._get_reg(op1))
+            else:
+                raise ValueError("Invalid instruction")
+        else:
+            raise ValueError("Invalid number of operands")
+        return False
+
+    def get_state(self) -> tuple[int, int, int, int]:
+        return (self.rdi, self.rsi, self.rax, self.cmp_res)
